6ddf6c7540e58da0729929f0694a6b239d5efa25
Akshay Chavan <akshayc278@gmail.com>
2022-05-08 19:09:12 +0530

    Shell Script Basics

---
+++ day1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script is testing script for linux environment
+
+echo "I don't have to be greate to start, but I have to start to be great!"
+VARIABLE="Akshay"
+echo "hi ${VARIABLE}"
+
+#Reassign variable
+VARIABLE="Kshitija"
+echo "hi ${VARIABLE}"
